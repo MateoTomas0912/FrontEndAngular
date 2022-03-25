2c80879ea52a7440cd9d790a847b27f59090e312
Mateo Tomas <mtomas216crp@gmail.com>
2022-03-24 23:41:24 -0300

    segundoCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ExperienciasComponent } from './components/experiencias/experiencias.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PerfilComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ExperienciasComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
